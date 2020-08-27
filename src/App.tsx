5b060e12bc2a36e7a16f10826787f92be99c2ee9
Ajith20899 <ajithsachinrohit@gmail.com>
2020-08-27 19:29:13 +0530

    text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import ReactQuill from 'react-quill';
+import "./App.css";
 
-function App() {
+// type TextEditorT = {
+//   onEditorChange: (t: string, h: string) => void;
+//   boxTextEditor?: boolean;
+// };
+
+export const TextEditor = () => {
+  // const { onEditorChange, boxTextEditor } = props;
+
+  const [text, setText] = useState("");
+  const reactQuillRef = useRef() as any;
+  const boxTextEditor = false;
+
+  useEffect(() => {
+    let quill = reactQuillRef && reactQuillRef.current;
+    if (!quill) return;
+    if (quill) {
+      const editor = quill.getEditor();
+      const unprivilegedEditor = quill.makeUnprivilegedEditor(editor);
+      console.log(unprivilegedEditor);
+      // onEditorChange(unprivilegedEditor.getText(), text);
+    }
+  }, [text]);
+
+  const handleChange = (value: React.SetStateAction<string>) => {
+    setText(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactQuill
+      ref={reactQuillRef}
+      value={text}
+      onChange={handleChange}
+      theme={"snow"}
+      id={boxTextEditor ? "boxTextEditor" : ""}
+      modules={TextEditor.modules}
+      formats={TextEditor.formats}
+    />
   );
-}
+};
 
-export default App;
+TextEditor.modules = {
+  toolbar: [
+    [{ 'header': [6, 5, 4, 3, 2, 1, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'code-block'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+    [{ 'color': [] }, { 'background': [] }, 'clean'],
+  ],
+  clipboard: {
+    matchVisual: false,
+  }
+}
+/*
+ * Quill TextEditor formats
+ * See https://quilljs.com/docs/formats/
+ */
+TextEditor.formats = [
+  'header',
+  'bold', 'italic', 'underline', 'strike',
+  'code-block',
+  'list', 'indent',
+  'color', 'background',
+  'clean'
+]
